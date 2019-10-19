3997936912d03470940fc9f6571cb702763f943b
Dan Collins <dan@collinsnz.com>
2019-10-19 06:16:36 +0000

    Updated to use custom version information
    The custom version is cool, because it makes is easy to see what firmware is
    running on my board. Obviously, this was useful to make sure that the code I
    compiled was actually running (without having to modify some LED somewhere).
    
    I noticed the '__has_include()' macro wasn't working right. Notice we check it
    exists first (which it does), and then try to test the header
    exists. Unfortunately, we're missing the '../../' when we test - so the compiler
    can't find our header.
    
    This change assumes the header is there if a custom version file was enabled. A
    compiler error, in my opinion, is much more appropriate as silently ignoring when
    I explicitly ask for a custom header is confusing!
    
    Note the quick test this works after building:
    
      strings firmware.bin | grep dancollins # ... or whatever custom info you added

--- Marlin/src/inc/MarlinConfigPre.h
+++ Marlin/src/inc/MarlinConfigPre.h
@@ -36,15 +36,8 @@
 #include "../core/macros.h"
 #include "../../Configuration.h"
 
-
 #ifdef CUSTOM_VERSION_FILE
-  #if defined(__has_include)
-    #if __has_include(CUSTOM_VERSION_FILE)
-      #include XSTR(../../CUSTOM_VERSION_FILE)
-    #endif
-  #else
-    #include XSTR(../../CUSTOM_VERSION_FILE)
-  #endif
+  #include XSTR(../../CUSTOM_VERSION_FILE)
 #endif
 
 #include "Version.h"
